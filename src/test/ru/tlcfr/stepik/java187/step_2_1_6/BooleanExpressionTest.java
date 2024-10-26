e30eb564f76de04b9b00839fffec8f8b7ced413c
tlcfr <tatarinov.lcfr@gmail.com>
2024-10-26 23:12:03 +0300

    step_2_1_6

---
+++ src/test/ru/tlcfr/stepik/java187/step_2_1_6/BooleanExpressionTest.java
@@ -0,0 +1,24 @@
+package ru.tlcfr.stepik.java187.step_2_1_6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.tlcfr.stepik.java187.step_2_1_6.BooleanExpression.booleanExpression;
+
+class BooleanExpressionTest {
+
+    @Test
+    void sampleInput1() {
+        assertFalse(booleanExpression(false, false, false, false));
+    }
+
+    @Test
+    void sampleInput2() {
+        assertFalse(booleanExpression(true, true, true, true));
+    }
+
+    @Test
+    void sampleInput3() {
+        assertTrue(booleanExpression(false, false, true, true));
+    }
+}
